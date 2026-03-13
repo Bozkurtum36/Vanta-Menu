68f28450e04ed3fc38f5d7431cd6d4e3130b3f27
Bozkurtum36 <gurkan.guzeldere@icloud.com>
2026-03-13 22:16:51 +0100

    auth.ts

---
+++ auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import Discord from "next-auth/providers/discord"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID!,
+      clientSecret: process.env.AUTH_DISCORD_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub!
+      }
+      return session
+    },
+  },
+})
